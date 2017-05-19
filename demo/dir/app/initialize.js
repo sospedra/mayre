249463edaa2b684db805a85aa3b58e03d2ea34d4
sospedra <sospedra.r@gmail.com>
2017-05-19 12:20:22 +0200

    chore: Replace brunch demo with create-react-app

--- demo/dir/app/initialize.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from 'components/App';
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.querySelector('#app'));
-});
