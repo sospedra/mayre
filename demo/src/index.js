f2e522061f03d8d436a99a76e95e455ad9ef8bb3
Rubén Sospedra <sospedra.r@gmail.com>
2019-11-22 12:18:05 +0100

    chore: Update demo versions

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
